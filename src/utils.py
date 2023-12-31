628ed2054489050a9adab86c5c91a814bbceb384
Pranav Pahilwan <122338049+pranavpahilwan@users.noreply.github.com>
2023-12-31 13:03:41 +0530

    Add files via upload

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
 
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+
 def sav_object(file_path, obj):
     try:
         dir_path=os.path.dirname(file_path)
@@ -15,4 +17,26 @@ def sav_object(file_path, obj):
             pickle.dump(obj,file_obj)
 
     except Exception as e:
+        raise CustomException(e,sys)
+
+def evaluate_model(X_train, y_train, X_test, y_test,models):
+    try:
+        report={}
+        for i in range(len(models)):
+            model=list(models.values())[i]
+            model.fit(X_train,y_train)
+
+            #Predict testing data
+            y_test_pred=model.predict(X_test)
+
+            #Get r2_score for train test data
+            #train_model_score=r2_score(y_train,y_train_pred)
+            test_model_score=r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]]=test_model_score
+
+        return report
+        
+    except Exception as e:
+        logging.info('Exception occured during model training')
         raise CustomException(e,sys)
