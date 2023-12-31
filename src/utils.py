98631120ed7d9a61804b2bbeb0fe5baf80a9f36a
Pranav Pahilwan <122338049+pranavpahilwan@users.noreply.github.com>
2023-12-31 18:16:56 +0530

    Add files via upload

--- src/utils.py
+++ src/utils.py
@@ -38,5 +38,15 @@ def evaluate_model(X_train, y_train, X_test, y_test,models):
         return report
         
     except Exception as e:
-        logging.info('Exception is occured during model training')
+        logging.info('Exception occured during model training')
         raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return pickle.load(file_obj)
+    
+    except Exception as e:
+        logging.info('Exception occured in load_object function utils')
+        raise CustomException(e,sys)
+    
